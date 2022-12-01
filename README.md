4e174704ebc5964c1b574ab30e3e91035bd2ef37
Alex Sadler <rookwood101@users.noreply.github.com>
2022-12-01 01:05:19 +0000

    Significant rework to be more performant and focused
    Incl smart detection of loss of power using magic number

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # clean-reminder
  
+You have an ESP-12E
+https://en.wikipedia.org/wiki/ESP8266
+https://www.mischianti.org/2021/05/04/esp-12-high-resolution-pinout-and-specs/
+https://components101.com/sites/default/files/component_datasheet/ESP12E%20Datasheet.pdf
+https://www.instructables.com/Ultimate-ESP8266-01-Programmer-USB-Adapter/
+
+usb ESP-8266 programmer:
+https://smile.amazon.co.uk/gp/product/B078J7LDLY/ref=ppx_yo_dt_b_asin_title_o05_s00?ie=UTF8&psc=1
+can't find schematics for it
+you have a couple of photos in google photos that show how to plug it in
+red x x blue
+green black orange white ⬆
+
+how to run:
+plug into usb
+open serial monitor in arduino ide
+Select Generic ESP8266 module for port
+Both NL & CR, 115200 baud I think
+Pressing buton on breadboard seems to reset
+
+How to program:
+Connect yellow GPIO0 wire that dangles to negative (ground)
+hold down button on usb-to-serial while plugging it in and let go after 1 second
+Press the button on breadboard (reset) and then release and disconnect yellow GPIO0 wire
+Press upload
+https://www.instructables.com/3-Simple-Ways-of-Programming-an-ESP8266-12X-Module/
+when done disconnect yellow wire, unplug usb and plug back in
+
+# TODO
+ - [ ] Use performant types (not uint64_t)
+ - [ ] Wrap serial output in ifdef to avoid overhead when not debugging
+ - [ ] Measure current draw
+ - [ ] Measure performance and see if any optimisations needed
+ - [ ] Get whatsapp messages from dad for datasheets and schematics
--- clean-reminder.ino
+++ clean-reminder.ino
@@ -1,5 +1,6 @@
 #include <Esp.h>
-#include<stdint.h>
+#include <stdint.h>
+#define CLEAN_REMINDER_DEBUG 1
 
 // Deep-sleep for specified amount of hours, one hour at a time.
 // If powered on (not a deep-sleep reset), nothing will happen.
@@ -8,17 +9,17 @@
 
 
 // memory offsets, not actual values
-#define TOTAL_SLEEP 0
-#define CURRENT_SLEEP 8
+#define ADDR_MAGIC_NUMBER 8
 #define SLEEP_CYCLE 16
 
 // With larger values, deep-sleep is unrealiable: it might never wake up and consume a lot of power.
 // Therefore sleep one hour at a time.
 // In reality, the ESP sleeps a bit less than the 60 minutes it is told to.
-const static uint32_t MAX_SLEEP_CYCLE = 3600e6; // 1 hour in microseconds
-const static uint64_t TOTAL_SLEEP_DURATION = 4*3600e6; // 4 hours in microseconds
-// const static uint32_t MAX_SLEEP_CYCLE = 10e6; // 10 seconds in microseconds
-// const static uint64_t TOTAL_SLEEP_DURATION = 40e6; // 40 seconds in microseconds
+const static uint32_t MAX_SLEEP = 3600e6; // 1 hour in microseconds
+const static uint32_t LONG_SLEEP = 20e6; // 10 seconds in microseconds
+const static uint32_t SHORT_SLEEP = 5e6; // 1 seconds in microseconds
+const static uint64_t LED_AFTER_CYCLE = 3; // show LED after cycle 3
+const static uint64_t MAGIC_NUMBER = 584128;
 
 
 void rtcWrite64(uint32_t offset, uint64_t new_value) {
@@ -31,62 +32,59 @@ uint64_t rtcRead64(uint32_t offset) {
     return value;
 }
 
-void onWake() {
+void onWake(uint64_t cycles) {
+    if (rtcRead64(ADDR_MAGIC_NUMBER) != MAGIC_NUMBER) {
+      resetSleep();
+    }  
     bool waking_from_sleep = ESP.getResetReason() == "Deep-Sleep Wake";
     
     if (waking_from_sleep) {
-        uint64_t stored_sleep = rtcRead64(CURRENT_SLEEP);
-        uint64_t sleep_cycle = rtcRead64(SLEEP_CYCLE);
-        rtcWrite64(CURRENT_SLEEP, stored_sleep + sleep_cycle);
-        Serial.print("Waking up from deep-sleep via reset pin. Current sleep duration: ");
-        Serial.println(stored_sleep + sleep_cycle);
+        Serial.print("Waking up from deep-sleep via reset pin. Current number of sleep cycles: ");
+        Serial.println(cycles);        
     } else {
-        Serial.println("Waking up by some other means");
+        // not sure how to wake up any other way
+        Serial.println("Waking up by some other means. Resetting");
         resetSleep();
     }
 
 }
 
-bool isItTimeYet(uint64_t total_sleep_s) {
+bool isItTimeYet(uint64_t cycles) {
     // TODO this is not working
-    uint64_t current_sleep = rtcRead64(CURRENT_SLEEP);
-    Serial.println("Is it time yet? Checking if current sleep (" + String(current_sleep) + ") >= total sleep (" + String(total_sleep_s) + ")");
-    return current_sleep >= total_sleep_s;
+    Serial.println("Is it time yet? Checking if cycles (" + String(cycles) + ") >= led cycle (" + String(LED_AFTER_CYCLE) + ")");
+    return cycles >= LED_AFTER_CYCLE;
 }
 
 void resetSleep() {
     Serial.println("Resetting sleep. Current sleep duration: 0");
-    rtcWrite64(CURRENT_SLEEP, 0);
+    rtcWrite64(SLEEP_CYCLE, 0);
+    rtcWrite64(ADDR_MAGIC_NUMBER, MAGIC_NUMBER);
+    sleep(100, 0);
 }
 
-void sleep(uint64_t total_sleep_s) {
-    uint64_t current_sleep = rtcRead64(CURRENT_SLEEP);
-    if (current_sleep < total_sleep_s) {
-        uint32_t next_sleep_cycle = min(total_sleep_s - current_sleep, (uint64_t)MAX_SLEEP_CYCLE);
-        rtcWrite64(SLEEP_CYCLE, next_sleep_cycle);
-        Serial.println("Going to deep-sleep for " + String(next_sleep_cycle) + " seconds.");
-        ESP.deepSleep(next_sleep_cycle, WAKE_RF_DISABLED);
-    }
+void sleep(uint64_t duration, uint64_t current_cycles) {
+    rtcWrite64(SLEEP_CYCLE, current_cycles + 1);
+    Serial.println("Going to deep-sleep for " + String(duration) + " micro seconds.");
+    ESP.deepSleep(duration, WAKE_RF_DISABLED);
 }
 void setup() {
     Serial.begin(115200);
     Serial.println();
     Serial.println("Begin");
-    onWake();
-    if (isItTimeYet(TOTAL_SLEEP_DURATION)) {
+    uint64_t cycles = rtcRead64(SLEEP_CYCLE);
+    onWake(cycles);
+    if (isItTimeYet(cycles)) {
         Serial.println("complete!");
-        resetSleep();
-        pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-        return; // go to loop
+        // no need to trigger LED manually as it flickers a little on reset
+        // pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+        // digitalWrite(LED_BUILTIN, cycles % 2 == 0 ? LOW : HIGH);
+        sleep(SHORT_SLEEP, cycles);
+        // never gets here
     }
-    sleep(TOTAL_SLEEP_DURATION);
+    sleep(LONG_SLEEP, cycles);
     // never gets here
 }
 
 void loop() {
-    // LED turns on on LOW
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+  // nothing
 }
